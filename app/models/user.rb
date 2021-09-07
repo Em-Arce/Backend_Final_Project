6d3508d6657ad7f51f7a2eaa82d55e1de9cfee2b
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-07 18:55:12 +0800

    Adds prefix TYPE enum and validation for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  TYPES = %w(Dr. Ms. Mr. Mrs. Prof. Sir Dame)
+  validates :prefix, :inclusion => {:in => TYPES}
 end
