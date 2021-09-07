5c7c4a916c453a00073bc78cf0d54394588a4079
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-07 21:08:33 +0800

    Adds validations for reg fields and for password reqt

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
 
   TYPES = %w(Dr. Ms. Mr. Mrs. Prof. Sir Dame)
   validates :prefix, :inclusion => {:in => TYPES}
+  validates :first_name,:last_name, :email, presence: true, on: :create
+
+  PASSWORD_REQUIREMENTS = /\A #Start of string
+    (?=.{6,}) # at least 6 characters long
+    (?=.*\d)  # have at least one number
+    (?=.*[a-z]) # have at least one lowercase letter
+    (?=.*[A-Z]) # have at least one uppercase letter
+    (?=.*[[:^alnum:]]) # have at least one symbol
+  /x #End of string
+  validates :password, format: PASSWORD_REQUIREMENTS
+  validates :password_confirmation, format: PASSWORD_REQUIREMENTS
 end
