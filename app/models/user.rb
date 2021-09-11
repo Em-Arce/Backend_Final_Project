e3a2de1cb111dc9064f67da7d74ee21cea0f8a6a
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-11 19:41:45 +0800

    Adds validation for presence of prefix, adds more validation for password and password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,22 @@ class User < ApplicationRecord
 
   TYPES = %w(Dr. Ms. Mr. Mrs. Prof. Sir Dame)
   validates :prefix, :inclusion => {:in => TYPES}
-  validates :first_name,:last_name, :email, presence: true, on: :create
+  validates :prefix,:first_name,:last_name, :email, :password, :password_confirmation,
+            presence: true, on: :create
 
-  PASSWORD_REQUIREMENTS = /\A #Start of string
-    (?=.{6,}) # at least 6 characters long
-    (?=.*\d)  # have at least one number
-    (?=.*[a-z]) # have at least one lowercase letter
-    (?=.*[A-Z]) # have at least one uppercase letter
+  PASSWORD_FORMAT = /\A #Start of string
+    (?=.{6,}) # at least 6 characters
+    (?=.*\d)  # at least one number
+    (?=.*[a-z]) # at least one lowercase letter
+    (?=.*[A-Z]) # at least one uppercase letter
     (?=.*[[:^alnum:]]) # have at least one symbol
   /x #End of string
-  validates :password, format: PASSWORD_REQUIREMENTS
-  validates :password_confirmation, format: PASSWORD_REQUIREMENTS
+
+  #check the password complexity on create
+  validates :password, length: { in: Devise.password_length }, format: PASSWORD_FORMAT,
+             confirmation: true, on: :create
+
+  validates :password_confirmation, format: PASSWORD_FORMAT, on: :create
 
   #callback for sending email notification
   after_create :send_email
