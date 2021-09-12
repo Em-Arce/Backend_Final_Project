d3c7fc26676acc2d1b6f96fa840e30f2037fc26e
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-12 21:05:50 +0800

    Adds code for user creation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    prefix { 'Prof.' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 6, max_length: 128,
+          mix_case: true, special_characters: true) }
+    password_confirmation { "#{password}"}
+    admin { 'false' }
   end
 end
