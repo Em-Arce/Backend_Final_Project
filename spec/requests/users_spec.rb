99d5078087e1121c5e417a32908f43b7a8cabcc7
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-12 21:07:58 +0800

    Writes spec for request users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
-      expect(response).to have_http_status(:success)
+  describe "GET #index when admin is signed in" do
+    it "should have status 200" do
+      user_admin = create(:user, admin: "true")
+      sign_in user_admin
+      get users_path
+      expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
+  describe "GET #static_pages_welcome when user who is not admin is signed in" do
+    it "should have status 302" do
+      user = create(:user)
+      sign_in user
+      get users_path
+      expect(response).to have_http_status(302)
     end
   end
-
 end
