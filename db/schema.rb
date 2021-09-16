653a91a473bd9a6b95c734cf364fb102f055087d
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-16 21:29:43 +0800

    Removes foreign key from abstracts and conference foreign key from participations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_113924) do
+ActiveRecord::Schema.define(version: 2021_09_16_112010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,8 @@ ActiveRecord::Schema.define(version: 2021_09_14_113924) do
     t.string "main_author"
     t.string "co_authors"
     t.text "body"
-    t.bigint "participation_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["participation_id"], name: "index_abstracts_on_participation_id"
   end
 
   create_table "conferences", force: :cascade do |t|
@@ -43,10 +41,8 @@ ActiveRecord::Schema.define(version: 2021_09_14_113924) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "conference_id", null: false
     t.bigint "abstract_id", null: false
     t.index ["abstract_id"], name: "index_participations_on_abstract_id"
-    t.index ["conference_id"], name: "index_participations_on_conference_id"
     t.index ["user_id"], name: "index_participations_on_user_id"
   end
 
@@ -72,8 +68,6 @@ ActiveRecord::Schema.define(version: 2021_09_14_113924) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "abstracts", "participations"
   add_foreign_key "participations", "abstracts"
-  add_foreign_key "participations", "conferences"
   add_foreign_key "participations", "users"
 end
