6083a3ae87e7a0dce28898ef49b6e605018085a8
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-17 23:18:19 +0800

    Adds new and create methods and refactored

--- app/controllers/abstracts_controller.rb
+++ app/controllers/abstracts_controller.rb
@@ -1,10 +1,30 @@
 class AbstractsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user
+
+  def new
+    @user = current_user
+    @abstract = @user.abstracts.build
+  end
+
+  def create
+    @user = current_user
+    @abstract = @user.abstracts.create(abstract_params)
+
+    data = []
+    params[:abstract][:co_authors].reject(&:empty?).each do |id|
+      if id !=nil || id !=""
+        data << User.find(id)
+        @abstract.update!(co_authors: data)
+      end
+    end
+    #binding.pry
+    redirect_to user_path(current_user.id), notice: "Abstract submission completed."
+
+  end
 
   private
-  def set_user
-      @user = User.find(params[:id])
-      @participations = @user.participations.all
+
+  def abstract_params
+    params.require(:abstract).permit(:title, :main_author, :co_authors, :body)
   end
 end
