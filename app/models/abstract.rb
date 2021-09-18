29a4888ae45d0c627c4990767cd2bdd3b658d1b2
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-18 14:20:04 +0800

    Adds format_params_field method

--- app/models/abstract.rb
+++ app/models/abstract.rb
@@ -1,4 +1,29 @@
 class Abstract < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :users, through: :participations, dependent: :destroy
+
+  def format_params_field(name, params_fields, abstract)
+    data =[]
+    case name
+    when :co_authors
+      params_fields.reject(&:empty?).each do |params_field|
+        data << params_field
+      end
+      abstract.update!(co_authors: data)
+    when :references
+      params_fields.split("\r\n").each do |params_field|
+        data << params_field
+        #binding.pry
+      end
+      abstract.update!(references: data)
+    when :keywords
+      params_fields.split(",").reject(&:empty?).each do |params_field|
+        data << params_field
+        #binding.pry
+      end
+      abstract.update!(keywords: data)
+    else
+      puts "Please edit your abstract."
+    end
+  end
 end
