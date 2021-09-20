f6fe3be774acadb87391515a97a18db65271ee54
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-20 16:53:25 +0800

    Adds city, country and position_others to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_034230) do
+ActiveRecord::Schema.define(version: 2021_09_20_061439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,9 @@ ActiveRecord::Schema.define(version: 2021_09_18_034230) do
     t.boolean "isvsp2022_registered", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "city"
+    t.string "country"
+    t.string "position_other"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
