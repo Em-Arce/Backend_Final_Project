110111e975b529b5111c5b37458761fa7e6c81e2
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-20 22:25:58 +0800

    Adds position as enum kind and codes for nested attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,20 +35,43 @@ class User < ApplicationRecord
   #has_many :conferences, through: :participations, dependent: :destroy
   has_many :abstracts, through: :participations, dependent: :destroy
 
-  accepts_nested_attributes_for :abstracts
+  accepts_nested_attributes_for :participations
 
   validates :prefix,:first_name,:last_name, :email, :position,
             :university_institute_company, :department, :contact_number,
             presence: true, on: :update
 
-  def abstracts_attributes=(abstracts_attributes)
-    abstracts_attributes.values.each do |abstract_attribute|
-      abstract = Abstract.find_or_create_by(abstract_attribute)
-      self.abstracts << abstract
+  def participations_attributes=(participations_attributes)
+    participations_attributes.values.each do |participation_attribute|
+      participation = Participation.find_or_create_by(participation_attribute)
+      self.participations << participation
     end
   end
 
   def co_author_properties
     "#{prefix} #{first_name} #{last_name} #{suffix}"
   end
+
+  POSITIONS = { reseach_scientist: "Reseach Scientist",
+                professor: "Professor",
+                phd: "PhD",
+                faculty: "Faculty",
+                doctoral_student: "Doctoral Student",
+                masteral_student: "Masteral Student",
+                college_student: "College Student",
+                highschool_student: "High School Student",
+                other: "Other"
+  }
+  #this saves sa nil in db :((
+  # POSITIONS = { reseach_scientist: 0,
+  #               professor: 1,
+  #                  phd: 2,
+  #                  faculty: 3,
+  #                  doctoral_student: 4,
+  #                  masteral_student: 5,
+  #                  college_student: 6,
+  #                  highschool_student: 7,
+  #                  other: 8
+  #                   }
+  enum position: POSITIONS
 end
