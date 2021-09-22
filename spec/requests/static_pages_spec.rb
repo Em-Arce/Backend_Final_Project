08a671bc2061d486bb904b2dea42845dae28d4d5
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-22 21:58:30 +0800

    Refactors request spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,9 @@ RSpec.describe "StaticPages", type: :request do
 
   describe "GET /welcome" do
     it "should have status 200" do
-      get "/static_pages/welcome"
+      # user = create(:user)
+      # sign_in user
+      get static_pages_welcome_path
       expect(response).to have_http_status(200)
     end
   end
