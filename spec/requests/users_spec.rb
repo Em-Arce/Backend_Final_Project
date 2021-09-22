69d69ab39d17323421517bf9951752568081615c
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-22 21:59:15 +0800

    Adds request spec for nested resources

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,12 +10,37 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  describe "GET #static_pages_welcome when user who is not admin is signed in" do
+  describe "GET #index when user who is not admin is signed in" do
     it "should have status 302" do
       user = create(:user)
       sign_in user
       get users_path
       expect(response).to have_http_status(302)
+    end
+  end
+
+  #nested resource due to association
+  describe "GET participations#profile" do
+    it "should have status 200" do
+      user = create(:user, position: "research_scientist")
+      abstract = create(:abstract)
+      participation = build(:participation)
+      participation.update!(user_id: user.id, abstract_id: abstract.id)
+      sign_in user
+      get user_participation_profile_path(user.id, participation.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET participations#edit" do
+    it "should have status 200" do
+      user = create(:user, position: "research_scientist")
+      abstract = create(:abstract)
+      participation = build(:participation)
+      participation.update!(user_id: user.id, abstract_id: abstract.id)
+      sign_in user
+      get edit_user_participation_path(user.id, participation.id)
+      expect(response).to have_http_status(200)
     end
   end
 end
