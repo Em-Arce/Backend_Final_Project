78cd7ebe40fba9a354dfc2bc0ae172b66db3b8a1
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-22 16:57:11 +0800

    Adds code to change all participation kinds upon update and changes redirect path upon update

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -7,6 +7,12 @@ class ParticipationsController < ApplicationController
     @participation = @user.participations.find(params[:id])
   end
 
+  def profile
+    @user =  User.find(params[:user_id])
+    @abstracts = @user.abstracts.all
+    @participation = @user.participations.find(params[:participation_id])
+  end
+
   def edit
     @user =  User.find(params[:user_id])
     @participation = @user.participations.find(params[:id])
@@ -14,8 +20,18 @@ class ParticipationsController < ApplicationController
 
   def update
     @user =  User.find(params[:user_id])
-    #@abstract = @user.abstracts.first
     @participation = @user.participations.find(params[:id])
+    @participations = @user.participations.all
+
+
+
+    #for multiple abstracts hence multiple participations, the changes in
+    #one participation is cascaded to all participations
+    #@participation.set_kind(params[:user_id], params[:participation][:kind], params[:id])
+    @participations.each do |participation|
+      participation.update!(kind: params[:participation][:kind])
+      @participation.set_fee
+    end
 
     #update user details without going to another link
     prefix = params[:participation][:prefix]
@@ -32,9 +48,10 @@ class ParticipationsController < ApplicationController
       university_institute_company: university_institute_company,
       department: department, contact_number: contact_number)
 
+
     respond_to do |format|
       if @participation.update(participation_params)
-        format.html { redirect_to user_participation_path(@user, @participation),
+        format.html { redirect_to user_participation_profile_path(@user, @participation),
           notice: "Your participation details successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
