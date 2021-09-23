346c8269b06015801a55b80bfb8b0202808b7b4f
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-23 22:13:59 +0800

    Adds spec for custom method, presence of additional attributes on update and position as enum

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,38 @@ RSpec.describe User, type: :model do
     it { expect(subject).to validate_presence_of(:email).on(:create) }
     it { expect(subject).to validate_presence_of(:password).on(:create) }
     it { expect(subject).to validate_presence_of(:password_confirmation).on(:create) }
+
+    #check if position has any of the values in Positions enum
+    #source: https://github.com/thoughtbot/shoulda-matchers/blob/master/lib/shoulda/matchers/active_record/define_enum_for_matcher.rb
+    it do
+        should define_enum_for(:position).
+          with_values(
+            research_scientist: "Research Scientist",
+              professor: "Professor",
+              phd: "PhD",
+              faculty: "Faculty",
+              doctoral_student: "Doctoral Student",
+              masteral_student: "Masteral Student",
+              college_student: "College Student",
+              highschool_student: "High School Student",
+              other: "Other"
+          ).
+          backed_by_column_of_type(:string)
+    end
+
+    #check presence of fields on update
+    it { expect(subject).to validate_presence_of(:position).on(:update) }
+    it { expect(subject).to validate_presence_of(:university_institute_company).on(:update) }
+    it { expect(subject).to validate_presence_of(:department).on(:update) }
+    it { expect(subject).to validate_presence_of(:contact_number).on(:update) }
+    it { expect(subject).to validate_presence_of(:city).on(:update) }
+    it { expect(subject).to validate_presence_of(:country).on(:update) }
   end
 
+  describe '#co_author_properties' do
+    it 'returns value with valid data' do
+      user = create(:user, position: "research_scientist")
+      expect(user.co_author_properties).to eq("#{user.prefix} #{user.first_name} #{user.last_name} #{user.suffix}")
+    end
+  end
 end
