7048af2407f8323189f9e0b0e0636ccd8f23c47c
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-24 21:47:57 +0800

    Adds initial model spec

--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-# RSpec.describe Participation, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Participation, type: :model do
+  describe 'validations' do
+    # Here we're using FactoryBot, but you could use anything
+    subject { build(:participation) }
+
+    #check presence of fields on create
+    it { expect(subject).to validate_presence_of(:kind).on(:create) }
+    it { expect(subject).to validate_presence_of(:fee).on(:create) }
+
+    it do
+        should define_enum_for(:kind).
+          with_values(
+            regular_local_new_member: "Regular Local New Member",
+            regular_local: "Regular Local",
+            student_local: "Student Local",
+            regular_foreign: "Regular Foreign",
+            student_foreign: "Student Foreign",
+            invited_speaker: "Invited Speaker",
+            plenary_speaker: "Plenary Speaker",
+            exhibitor: "Exhibitor",
+            other: "Other"
+          ).
+          backed_by_column_of_type(:string)
+    end
+  end
+end
