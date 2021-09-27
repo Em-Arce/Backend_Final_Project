ad67968545c47a301e196318198481c71c725304
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-27 22:16:47 +0800

    Adds spec for custom method #format_affiliation

--- spec/models/abstract_spec.rb
+++ spec/models/abstract_spec.rb
@@ -36,6 +36,20 @@ RSpec.describe Abstract, type: :model do
     it { expect(subject.references).to have_at_least(1).items }
     it { expect(subject.references).to have_exactly(2).items }
   end
+
+  describe '#format_affiliation' do
+    it 'returns expected affiliation data' do
+      user = create(:user, prefix: "Ms.", position: "research_scientist")
+      co_author1 = create(:user, prefix: "Dr.", position: "phd")
+      co_author2 = create(:user, prefix: "Dr.", position: "faculty")
+      data = []
+      data << co_author1
+      data << co_author2
+      abstract = create(:abstract, main_author: "#{user.id}", co_authors: data)
+      #binding.pry
+      expect(abstract.format_affiliation(abstract.co_authors.map(&:symbolize_keys))).to eq(["#{co_author1.department}, #{co_author1.university_institute_company}, #{co_author1.city}, #{co_author1.country}", "#{co_author2.department}, #{co_author2.university_institute_company}, #{co_author2.city}, #{co_author2.country}"])
+    end
+  end
 end
 
 
