18efeb55d6b4d05853afb903c2a81edefbff42af
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-10-01 18:08:18 +0800

    Adds city and country to permitted parameters during sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ before_action :configure_devise_permitted_parameters, if: :devise_controller?
     # Add columns into the allowed parameter for sign up action
     devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name,
       :prefix, :suffix, :position, :university_institute_company,:department,
-      :contact_number, :admin, :isvsp2022_registered])
+      :contact_number, :city, :country, :admin, :isvsp2022_registered])
   end
 
   def after_sign_in_path_for(resource)
