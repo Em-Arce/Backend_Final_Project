c786249d246539bc2a1437b3637887179364ca2d
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-10-02 12:50:05 +0800

    Adds error handling here instead of in specific controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
 before_action :configure_devise_permitted_parameters, if: :devise_controller?
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  protected
+protected
 
   def configure_devise_permitted_parameters
     # Add columns into the allowed parameter for sign up action
@@ -14,4 +15,13 @@ before_action :configure_devise_permitted_parameters, if: :devise_controller?
     static_pages_welcome_path
   end
 
+  def render_unprocessable_entity_response(invalid)
+    #render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    redirect_to "/500.html" #404/422/500 -server error
+
+    ExceptionNotifier.notify_exception(
+      invalid,
+      env: request.env, data: { message: 'was doing something wrong' }
+    )
+  end
 end
