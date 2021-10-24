681f1e59ad60fdc7e3dcba4e88898636f89be908
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-10-24 19:56:33 +0800

    Adds devise confirmable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
 
   TYPES = %w(Dr. Ms. Mr. Mrs. Prof. Sir Dame).freeze
